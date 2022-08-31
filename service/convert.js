e9f3b0dbc2d5dad5bdcc418f930d82a6f40e142e
btopro <btopro@outlook.com>
2022-08-31 16:34:45 -0400

    margin

--- service/convert.js
+++ service/convert.js
@@ -121,7 +121,13 @@ export const getPdf = async (source, type = 'link') => {
 		displayHeaderFooter: true,
 		headerTemplate: '',
 		footerTemplate: '',
-		printBackground: true
+		printBackground: true,
+		margin: {
+			top: 20,
+			left: 20,
+			right: 20,
+			bottom: 20
+		}
 	})
 
 	// Close chrome instance
